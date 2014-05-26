8c2cc3a7f8f0ed5bf1fa3159c45ab6dadc2ff1e2
Márcio Almada <marcio3w@gmail.com>
2014-05-26 10:31:08 -0300

    add tests

---
+++ test/suite/StructTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Spruct;
+
+use Spruct\Fixture\Point;
+use Spruct\Fixture\DerivedPoint;
+use Spruct\Fixture\InvalidStruct;
+use Spruct\Fixture\StructWithRequirement;
+
+/**
+ * StructTest
+ *
+ * @group main
+ */
+class StructTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $struct;
+
+    public function setup() {
+        $this->struct = new Point();
+    }
+
+    /**
+     * @expectedException \Spruct\StructException
+     * @expectedExceptionMessage #^((\\?\w)+)->undeclared undefined \(Type \1 has no field undeclared\)$#
+     */
+    public function testFieldAccessException()
+    {
+        $this->struct->undeclared;
+    }
+
+    /**
+     * @dataProvider fieldAssignmentProvider
+     */
+    public function testFieldAssignment($property, $value, $type)
+    {
+        $this->struct->$property = $value;
+        $this->assertSame($value, $this->struct->$property);
+        $this->assertInternalType($type, $this->struct->$property);
+    }
+
+    public function fieldAssignmentProvider()
+    {
+        return [
+            ['visible', true,  'boolean'],
+            ['visible', false, 'boolean'],
+            ['x', 0.1, 'float'],
+            ['y', 0.1, 'float'],
+            ['group', 10, 'integer'],
+            ['description', 'I\'m a point', 'string'],
+            ['next', new Point(), 'object'],
+            ['previous', new Point(), 'object'],
+            ['previous', new DerivedPoint(), 'object'],
+            ['weak', [], 'array']
+        ];
+    }
+
+    public function testStructInitialization()
+    {
+        $point = new Point(["x" => 1.0, "y" => 1.5]);
+        $this->assertSame($point->x, 1.0);
+        $this->assertSame($point->y, 1.5);
+    }
+
+    /**
+     * @expectedException \Spruct\StructException
+     * @expectedExceptionMessage #^Cannot use ((\\?\w)+)(\(.{0,}\))? as type (?1) in field \w+$#
+     * @dataProvider fieldAssignmentExceptionProvider
+     */
+    public function testFieldAssignmentException($property, $value)
+    {
+        $this->struct->$property = $value;
+    }
+
+    public function fieldAssignmentExceptionProvider()
+    {
+        return [
+            ['visible', 1],
+            ['visible', 'yes'],
+            ['visible', ''], // empty string
+            ['visible', 'some # random % !    (string)'], // empty string
+            ['x', 1],
+            ['y', 0],
+            ['group', 1.0],
+            ['description', true],
+            ['next', new \stdClass()],
+            ['previous', new \stdClass()],
+        ];
+    }
+
+    /**
+     * @expectedException \Spruct\StructException
+     * @expectedExceptionMessage #^Annotated type must be in \[((")\w+(?2),?)+\]$#
+     */
+    public function testInvalidFieldType()
+    {
+        $this->struct = new InvalidStruct();
+        $this->struct->wrong = '';
+    }
+
+    /**
+     * @dataProvider propotypeProvider
+     * @expectedException \Spruct\StructException
+     * @expectedExceptionMessage #^Cannot initialize ((\\?\w)+) with a null \w+$#
+     */
+    public function testFieldRequirement($prototype)
+    {
+        $this->struct = new StructWithRequirement($prototype);
+    }
+
+    public function propotypeProvider()
+    {
+        return [
+            [[]],
+            [['description' => 'foo']],
+            [['name' => 'bar']],
+        ];
+    }
+}
